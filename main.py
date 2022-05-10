e3dd2b9f49c90d1fb8fc9872383afaa39d00f2f4
Carlota Martin-Anero <91574355+carlotamartin@users.noreply.github.com>
2022-05-10 07:06:35 +0000

    introducido el texto y los filósofos

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+from filosofo import *
 from ventana import window
 
-if __name__ == '__main__':
-  window()
+if __name__=="__main__":
+  window = window()
+  
+
+  lista=[]
+  for i in range(N):
+      lista.append(filosofo(window)) #AGREGA UN FILOSOFO A LA LISTA
+
+  for f in lista:
+      f.start() #ES EQUIVALENTE A RUN()
+  window.run()
+  for f in lista:
+      f.join() #BLOQUEA HASTA QUE TERMINA EL THREAD
--- ventana.py
+++ ventana.py
@@ -1,51 +1,38 @@
-from tkinter import *
+import tkinter as tk
 
 
 class window():
   def __init__ (self):
-    self.root = Tk()
+    self.root = tk.Tk()
     self.root.title('La cena de los filósofos')
     self.root.configure(bg = 'gainsboro')
     self.root.geometry('500x400')
 
 
     #Creamos a los filósofos
-    self.f1 = Label(self.root, text= 'Filósofo 01', bg = 'pale violet red', fg = 'black', width = 9, height = 2)
+    self.f1 = tk.Label(self.root, text= 'Filósofo 01', bg = 'pale violet red', fg = 'black', width = 9, height = 2)
     self.f1.place(x=85, y=30)
-    self.f1 = Label(self.root, text= 'Filósofo 02', bg = 'floral white', fg = 'black', width = 9, height = 2)
+    self.f1 = tk.Label(self.root, text= 'Filósofo 02', bg = 'floral white', fg = 'black', width = 9, height = 2)
     self.f1.place(x=160, y=75)
-    self.f1 = Label(self.root, text= 'Filósofo 03', bg = 'gold', fg = 'black', width = 9, height = 2)
-    self.f1.place(x=155, y=120)
-    self.f1 = Label(self.root, text= 'Filósofo 04', bg = 'pale violet red', fg = 'black', width = 9, height = 2)
+    self.f1 = tk.Label(self.root, text= 'Filósofo 03', bg = 'gold', fg = 'black', width = 9, height = 2)
+    self.f1.place(x=170, y=130)
+    self.f1 = tk.Label(self.root, text= 'Filósofo 04', bg = 'pale violet red', fg = 'black', width = 9, height = 2)
     self.f1.place(x=50, y=130)
-    self.f1 = Label(self.root, text= 'Filósofo 05', bg = 'gold', fg = 'black', width = 9, height = 2)
+    self.f1 = tk.Label(self.root, text= 'Filósofo 05', bg = 'gold', fg = 'black', width = 9, height = 2)
     self.f1.place(x=30, y=75)
 
 
-
-
-    self.canvas = Canvas(self.root)
-    self.canvas.place(x=9,y=150)
-    self.scrollbar = Scrollbar(self.canvas, orient = VERTICAL)
-    self.scrollbar.pack(side = RIGHT, fill = Y)
-
-    #Button(self.root, text = 'Filósofo 1', width = 50, bg = 'blue', fg = 'white').place(x=85, y =75)
-
-    '''
-    self.frame_f = Frame (self.root, width = 480, height = 320)
-    self.frame_f.pack(fill='both', expand=0.5)
-    self.frame_f.config(cursor="pirate")
-    self.frame_f.config(bg="lightblue")
-    self.frame_f.config(bd=25)
-    self.frame_f.config(relief="sunken")
-    '''
-
-
-
-
-
     
-    #self.frame_f.pack()
-  
+    self.texto = tk.Text(self.root, height=12, width = 35) #texto donde va a ir los filósofos pensando
+    self.scroll = tk.Scrollbar(self.root) #establecemos que nuestra ventana va a tener una barra deslizante
+    self.texto.configure(yscrollcommand = self.scroll.set) #configuramos netsra barra deslizante dentro de nuetro texto
+    self.texto.place(x = 10,  y =185)
+    self.scroll.config (command= self.texto.yview)
+    self.scroll.pack(side = tk.RIGHT, fill= tk.Y)
   
     self.root.mainloop()
+
+  def imprimir(self, texto):
+    self.texto.insert(tk.END, str(texto) + '\n')
+
+  
